ec1d32d7f05fa1533825e5db0b70d6fd6e87b774
Joshua Burger <joshberger6969@outlook.com>
2021-10-23 18:02:27 -0400

    array full of stings with messages

---
+++ src/Data.js
@@ -0,0 +1,38 @@
+
+const inspirationalMessages = [
+    {
+        "inspirationalMessage": "Act as if what you do makes a difference. It does."
+    },
+    {
+        "inspirationalMessage": "Success is not final, failure is not fatal: it is the courage to continue that counts."
+    },
+    {
+        "inspirationalMessage": "Never bend your head. Always hold it high. Look the world straight in the eye."
+    },
+    {
+        "inspirationalMessage": "What you get by achieving your goals is not as important as what you become by achieving your goals."
+    },
+    {
+        "inspirationalMessage": "Believe you can and you're halfway there."
+    },
+    {
+        "inspirationalMessage": "When you have a dream, you've got to grab it and never let go."
+    },
+    {
+        "inspirationalMessage": "Forget all the reasons why it won't work and believe the one reason why it will"
+    },
+    {
+        "inspirationalMessage": "Always do what you are afraid to do"
+    },
+    {
+        "inspirationalMessage": "Don’t be intimidated by what you don’t know. That can be your greatest strength and ensure that you do things differently from everyone else."
+    },
+    {
+        "inspirationalMessage": "If you keep on doing what you've always done, you will keep getting what you've always gotten."
+    },
+    {
+        "inspirationalMessage": "The surest way to find your dream job is to create it."
+    }
+]
+
+export default inspirationalMessages;
